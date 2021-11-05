166f18bda083f49a011e50425f2bb178f109a348
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-11-05 15:27:43 +0900

    {finish]Java_4章

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "Ichiyanagi";
+		System.out.println(str1);
+
+		str1 = "ONE PIECE";
+		System.out.println(str1);
+
+		final int NUM;
+		NUM = 7;
+		System.out.println(NUM);
+	}
+}
