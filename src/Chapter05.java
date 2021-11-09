5efcc4af99d528b08d99e1ed0a7accb3d727fa5d
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-11-09 17:12:01 +0900

    [finish]Java_5章

---
+++ src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 12345678L;
+		System.out.println(long1);
+
+		double double1 = 12345.6788;
+		System.out.println(double1);
+
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+
+		Float f = 1234.5678f;
+		System.out.println(f);
+
+		Integer i = 12345;
+		System.out.println(i);
+
+		// Characterは、シングルクォーターのみ有効
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 10, 20, 30, 40, 50 };
+		System.out.println(array[3]);
+	}
+}
