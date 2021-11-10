e7716cbffecadf7050b9100b046b97b5f53b6396
ichiyanagi7 <ichiyanagi1024@gmail.com>
2021-11-10 14:22:31 +0900

    [finish]Java_7章

---
+++ src/Chapter07.java
@@ -0,0 +1,15 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int l = 13;
+		if (l <= 5) {
+			System.out.println("とても近いです");
+		} else if (l <= 10) {
+			System.out.println("近いです");
+		} else if (l <= 15) {
+			System.out.println("遠いです");
+		} else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
